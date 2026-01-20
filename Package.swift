580ad47abf3cf2aea0d8258eedbebfb68598e128
sebastianwww <sebastiancole1215@icloud.com>
2026-01-20 16:09:19 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,12 +5,11 @@ let package = Package(
     name: "TerminalOS",
     platforms: [.iOS(.v15)],
     products: [
-        // Satisfies xtool's "No library products" error
-        .library(name: "TerminalOS", targets: ["TerminalOS"])
+        // 2026 FIX: Only define the executable here. 
+        .executable(name: "TerminalOS", targets: ["TerminalOS"])
     ],
     targets: [
-        // Using '.target' instead of '.executableTarget' stops the conflict
-        .target(
+        .executableTarget(
             name: "TerminalOS",
             path: "Sources/TerminalOS"
         )
