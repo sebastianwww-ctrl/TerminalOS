72c0bc40b1e7052846cb7180a284e9ec71482c1e
sebastianwww <sebastiancole1215@icloud.com>
2026-01-19 21:59:23 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ let package = Package(
     name: "TerminalOS",
     platforms: [.iOS(.v15)],
     products: [
+        // 2026 FIX: Vend both so xtool sees a library to build and an app to run
+        .library(name: "TerminalOSLib", targets: ["TerminalOS"]),
         .executable(name: "TerminalOS", targets: ["TerminalOS"])
     ],
     targets: [
