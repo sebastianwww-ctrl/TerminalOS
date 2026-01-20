7f6f95052fe2cf507be1e7afd22e62e4e21072b1
sebastianwww <sebastiancole1215@icloud.com>
2026-01-19 20:49:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ let package = Package(
     name: "TerminalOS",
     platforms: [.iOS(.v15)],
     products: [
-        .library(name: "TerminalOSLib", targets: ["TerminalOSLib"]),
         .executable(name: "TerminalOS", targets: ["TerminalOS"])
     ],
     targets: [
-        .target(name: "TerminalOSLib", path: "Sources/TerminalOSLib"),
-        .executableTarget(name: "TerminalOS", dependencies: ["TerminalOSLib"], path: "Sources/TerminalOS")
+        .executableTarget(
+            name: "TerminalOS",
+            path: "Sources/TerminalOS"
+        )
     ]
 )
-
