0b1658e1358798b78c8d30a3f5e722d4a78e0fe8
sebastianwww <sebastiancole1215@icloud.com>
2026-01-19 20:13:28 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,15 @@ import PackageDescription
 let package = Package(
     name: "TerminalOS",
     platforms: [.iOS(.v15)],
-    products: [.executable(name: "TerminalOS", targets: ["TerminalOS"])],
+    products: [
+        // 2026 Fix: Declare a library product so xtool can find 'library products'
+        .library(name: "TerminalOSLib", targets: ["TerminalOS"]),
+        .executable(name: "TerminalOS", targets: ["TerminalOS"])
+    ],
     targets: [
         .executableTarget(
-            name: "TerminalOS"
+            name: "TerminalOS",
+            path: "Sources/TerminalOS"
         )
     ]
 )
